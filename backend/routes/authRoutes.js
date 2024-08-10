41b87e06c723b11774a42b5ff036bbe4905a564d
subham2020btecs00002 <subhamcoolkumaryadav@gmail.com>
2024-08-11 00:35:16 +0530

    commit

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { registerUser, loginUser, getUser } = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.post('/register', registerUser);
+router.post('/register', registerUser);  
 router.post('/login', loginUser);
 router.get('/user', authMiddleware, getUser);
 
